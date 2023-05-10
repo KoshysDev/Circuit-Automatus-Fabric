e41a4bf55e11a7e359e667f46ef7454dad0aa878
Борис Кобылянский <koshys.osu@gmail.com>
2023-05-10 10:58:42 +0300

    Initial commit

---
+++ src/main/java/net/koshys/circuitautomatus/CircutAutomatusClient.java
@@ -0,0 +1,10 @@
+package net.koshys.circuitautomatus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CircutAutomatusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
